ba5eaef9c3a4c41dbc7333cfcb325668d15cec72
Fitnawati <38834410+Fitnawati24@users.noreply.github.com>
2018-05-29 09:53:16 +0700

    Add files via upload

---
+++ Perkalian8.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author WINDOWS 8.1 PRO
+ */
+public class Perkalian8 {
+    public static void main(String[] args) {
+        System.out.println("Tugas Struktur Data Membuat Perkalian 8");
+        System.out.println("Dengan Menggunakan For");
+        System.out.println("Nama : fitnawati");
+        System.out.println("Nim : E1E117030");
+        
+         int x = 8, y;
+        for(int i = 1;i <= 10; i++){
+            y = x * i;
+            System.out.println(x + " * " + i + " = " + y);
+           
+        }
+       
+        System.out.println("TERIMA KASIH");
+        
+         
+                
+                
+                
+                
+                
+    }
+    
+}
